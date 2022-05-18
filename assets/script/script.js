b542e3e36abf11c9ba6b32955dd3795d518749bc
eacquistapace <eacquistapace@gmail.com>
2022-05-17 23:27:30 -0600

    added timeblock timeTracker function

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,3 +2,29 @@
 var todayDate = moment();
 $("#currentDay").text(todayDate.format("MMMM Do YYYY"));
 console.log(todayDate);
+
+//Determines current time to change background for timeblocks (past, present, and future)
+function timeTracker() {
+  var timeNow = moment().hour();
+
+  $(".time-block").each(function () {
+    var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+    if (blockTime < timeNow) {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+    } else if (blockTime === timeNow) {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    }
+  });
+}
+
+//Call function
+timeTracker();
